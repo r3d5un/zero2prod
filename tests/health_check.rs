c269d5eaa611f96ebc5929dd3c92df95cfda4afc
r3d5un <r3d5un@pm.me>
2023-05-19 13:17:42 +0200

    Update database configuration

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,4 @@
 use once_cell::sync::Lazy;
-use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
@@ -10,14 +9,13 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
     let subscriber_name = "test".to_string();
-
     if std::env::var("TEST_LOG").is_ok() {
         let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
         init_subscriber(subscriber);
     } else {
         let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
         init_subscriber(subscriber);
-    }
+    };
 });
 
 pub struct TestApp {
@@ -34,12 +32,10 @@ async fn spawn_app() -> TestApp {
 
     let mut configuration = get_configuration().expect("Failed to read configuration.");
     configuration.database.database_name = Uuid::new_v4().to_string();
-
     let connection_pool = configure_database(&configuration.database).await;
 
     let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
-
     TestApp {
         address,
         db_pool: connection_pool,
@@ -48,21 +44,18 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create database
-    let mut connection =
-        PgConnection::connect(&config.connection_string_without_db().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres");
-
+    let mut connection = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to Postgres");
     connection
         .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
         .await
-        .expect("Failed to create database");
+        .expect("Failed to create database.");
 
     // Migrate database
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to Postgres.");
-
     sqlx::migrate!("./migrations")
         .run(&connection_pool)
         .await
@@ -79,6 +72,7 @@ async fn health_check_works() {
 
     // Act
     let response = client
+        // Use the returned application address
         .get(&format!("{}/health_check", &app.address))
         .send()
         .await
@@ -113,12 +107,12 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .await
         .expect("Failed to fetch saved subscription.");
 
-    assert_eq!(saved.email.unwrap(), "ursula_le_guin@gmail.com");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
-async fn subscribe_retruns_a_400_when_data_is_missing() {
+async fn subscribe_returns_a_400_when_data_is_missing() {
     // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
@@ -142,8 +136,10 @@ async fn subscribe_retruns_a_400_when_data_is_missing() {
         assert_eq!(
             400,
             response.status().as_u16(),
+            // Additional customised error message on test failure
             "The API did not fail with 400 Bad Request when the payload was {}.",
             error_message
         );
     }
 }
+
